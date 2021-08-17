c5f019902aa2cd1e3fb6713cce5f1a32b14f4c83
Sagar Lad <ladsagar919@gmail.com>
2021-08-17 15:17:30 +0530

    Added Welcome message to Employee Wage

---
+++ EmployeeWageSolution/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeWageSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Welcome To the Employee Wage Problem");
+            
+        }
+    }
+}
